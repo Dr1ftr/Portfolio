62af5524abcfb2234d379932c3a7dd32362cd44f
jasper de jong <jasperrijsb@gmail.com>
2022-05-28 00:02:43 +0200

    Initial commit + index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="theTop">
+        <div class="indexColumn">
+            <div class="mainName">
+                <h1>Jasper</h1>
+                <h1>Rijsbaarman</h1>
+            </div>
+            <h3 class="navbar"><a href="">-> About me</a></h3>
+        </div>
+        <img src="../img/download.png" alt="" class="indexImage">
+    </div>
+    
+</body>
+</html>
