6a7b452332c0e93ff3bb440a3688cdf496315ebb
jasper de jong <jasperrijsb@gmail.com>
2022-05-30 00:23:01 +0200

    redesigned index.php according to new wireframe

--- index.php
+++ index.php
@@ -4,20 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <title>Home</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <div class="theTop">
-        <div class="indexColumn">
-            <div class="mainName">
-                <h1>Jasper</h1>
-                <h1>Rijsbaarman</h1>
-            </div>
-            <h3 class="navbar"><a href="">-> About me</a></h3>
-        </div>
-        <img src="../img/download.png" alt="" class="indexImage">
+    <div class="theCenter">
+        <img src="../img/me.jpeg" alt="" class="avatar">
+        <h1 class="topText">Jasper Rijsbaarman</h1>
     </div>
-    
 </body>
 </html>
