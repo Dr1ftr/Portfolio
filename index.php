fb41b2841c24a0aa318dc52e65f6b73054a23543
jasper de jong <jasperrijsb@gmail.com>
2022-05-30 17:26:56 +0200

    fade out animation for switching views

--- index.php
+++ index.php
@@ -8,12 +8,29 @@
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <div class="theCenter">
+    <div class="index open" id="index">
         <img src="../img/me.jpeg" alt="" class="avatar">
         <h1>Jasper Rijsbaarman</h1>
         <div class="divider"></div>
         <h1><a href="../aboutMe.php">About me</a></h1>
         <h1><a href="">My work</a></h1>
+        <button onclick="switchView(index)">Test</button>
     </div>
 </body>
 </html>
+
+<script>
+    function switchView(divId, targetDivId) {
+        divId.classList.remove('open')
+        divId.classList.add('hide')
+        setTimeout(function(params) {
+            divId.classList.add('hidden')
+            divId.classList.remove('hide')
+        }, 3000);
+    }
+    function show(divId) {
+        divId.classList.remove('hide')
+
+        divId.classList.add('open')
+    }
+</script>
